6e1743ee58c72ae92281c836e78c1a8a2950661b
Erick Lima <erickcreimberg@gmail.com>
2024-10-09 15:33:58 -0300

    Initial Commit

---
+++ ArquiteturaSoftware/ArquiteturaSoftwareApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArquiteturaSoftwareApp.swift
+//  ArquiteturaSoftware
+//
+//  Created by ERICK COSTA REIMBERG DE LIMA on 09/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ArquiteturaSoftwareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArquiteturaSoftwareTests/ArquiteturaSoftwareTests.swift
@@ -0,0 +1,17 @@
+//
+//  ArquiteturaSoftwareTests.swift
+//  ArquiteturaSoftwareTests
+//
+//  Created by ERICK COSTA REIMBERG DE LIMA on 09/10/24.
+//
+
+import Testing
+@testable import ArquiteturaSoftware
+
+struct ArquiteturaSoftwareTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
